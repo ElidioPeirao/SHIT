620bd0bfdcd4294b2b84e3e7d72aff034f4437d1
ElidioPeirao <ElidioPeiraoJunior@gmail.com>
2025-05-01 22:28:02 -0300

    Update db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,7 @@
 import GoogleSheetsDB from './googleSheetsDB';
 
-/**
- * Aqui você cria UMA instância única do GoogleSheetsDB
- * apontando pro seu Web App no Google Sheets.
- */
 const googleDB = new GoogleSheetsDB({
-  sheetId: 'AKfycbwWnFMmxaPcA8HZBABZQi9pV-ZqFgYIEx0Fp0P4fNm4PcYY5P3L1Keszb-03DFIoypr'
+  sheetId: import.meta.env.VITE_SHEET_ID
 });
 
 export default googleDB;
